a24260422fe107359fdae0a41fd6e28c72c734c0
Bas van der Heijden <info@basvdheijden.com>
2018-04-11 15:46:32 +0200

    Make sure multisite drush commandos are supported

--- config/scripts/update.sh
+++ config/scripts/update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cd /var/www/web
-drush -y updb
-drush -y cim
-drush cr
+cd /var/www/web;
+
+for x in `ls /var/www/web/sites`; do
+  if [ -f "/var/www/web/sites/$x/settings.php" ]; then
+    drush -l $x -y updb
+    drush -l $x -y cim
+    drush -l $x cr
+  fi
+done
