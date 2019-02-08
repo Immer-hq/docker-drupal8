50668766fd6d5978555abe68d0424245949a005e
Maurits Lawende <mauritsl@users.noreply.github.com>
2019-02-08 13:28:31 +0100

    Clear cache before updb - see d.o. 3031128

--- config/scripts/update.sh
+++ config/scripts/update.sh
@@ -4,6 +4,7 @@ cd /var/www/web;
 
 for x in `ls /var/www/web/sites`; do
   if [ -f "/var/www/web/sites/$x/settings.php" ]; then
+    drush -l $x cr
     drush -l $x -y updb
     drush -l $x -y cim
     drush -l $x cr
