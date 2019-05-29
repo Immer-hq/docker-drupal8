fe945ee7797fe5b4c46c6564f8edfcaf033e35e7
Maurits Lawende <mauritsl@users.noreply.github.com>
2019-05-29 12:08:03 +0200

    Break update script on error

--- config/scripts/update.sh
+++ config/scripts/update.sh
@@ -5,8 +5,20 @@ cd /var/www/web;
 for x in `ls /var/www/web/sites`; do
   if [ -f "/var/www/web/sites/$x/settings.php" ]; then
     drush -l $x cr
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
     drush -l $x -y updb
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
     drush -l $x -y cim
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
     drush -l $x cr
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
   fi
 done
