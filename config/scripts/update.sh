ed8c4a8def813ef261b204b1d4983f3cc181781b
Maurits Lawende <mauritsl@users.noreply.github.com>
2019-11-11 15:35:49 +0100

    ADd option to skip webform imports

--- config/scripts/update.sh
+++ config/scripts/update.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
+set -e
+
 cd /var/www/web;
 
 for x in `ls /var/www/web/sites`; do
   if [ -f "/var/www/web/sites/$x/settings.php" ]; then
     drush -l $x cr
-    if [ $? -ne 0 ]; then
-      exit 1
-    fi
     drush -l $x -y updb
-    if [ $? -ne 0 ]; then
-      exit 1
+    if [ -n "$SKIP_WEBFORM_IMPORT" ]; then
+      mkdir -p /tmp/config-export/
+      drush config-export -l $x -y --destination=/tmp/config-export/
+      cp /tmp/config-export/webform.webform* /var/www/config/sync/
+      rm -Rf /tmp/config-export
     fi
     drush -l $x -y cim
-    if [ $? -ne 0 ]; then
-      exit 1
-    fi
     drush -l $x cr
-    if [ $? -ne 0 ]; then
-      exit 1
-    fi
     if [ -f "../translations/nl.po" ]; then
       drush language-import ../translations/nl.po
     fi
